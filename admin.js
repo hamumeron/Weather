f4676d10eabd261f446949acbc930dc9f6879b32
hamumeron <nekosuke.1012@outlook.jp>
2025-07-18 18:34:23 +0900

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,4 @@
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("fixedCode").innerText = FIXED_CODE;
+  document.getElementById("randomCode").innerText = RANDOM_CODE;
+});
